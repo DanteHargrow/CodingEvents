318f78e62c782a6b746518ad6caa50ad95a27215
dantehargrow <dantehargrow@gmail.com>
2020-12-07 23:19:10 -0600

    Added new event category controller

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEvents.ViewModels;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> eventsCategories = context.EventCategories.ToList();
+            return View(eventsCategories);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+        [HttpPost]
+        [Route("EventCategory/Create/")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory eventCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                context.EventCategories.Add(eventCategory);
+                context.SaveChanges();
+                return Redirect("/EventCategory/");
+            }
+            return View(addEventCategoryViewModel);
+        }
+    }
+}
